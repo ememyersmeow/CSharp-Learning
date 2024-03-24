8936bb9d8cf7c64fe637e8eefd8ebc02d4ed1348
Emma Myers <156679120+ememyersmeow@users.noreply.github.com>
2024-03-24 21:02:39 +0300

    Explicit implementation of interface members

---
+++ Abstract classes and interfaces/Explicit implementation of interface members.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+// first intraface
+interface First
+{
+    void show();
+}
+
+// second interface
+interface Second
+{ 
+    void show();
+}
+
+// class implements interfaces
+class MyClass: First, Second
+{
+    public void show()
+    {
+        Console.WriteLine("Class object MyClass");
+    }
+}
+
+// class with main method
+class ImplInterfaceDemo
+{
+    static void Main()
+    {
+        // create object
+        MyClass obj = new MyClass();
+        // the object reference is written to interface variables
+        First A = obj;
+        Second B = obj;
+        // method calls through different variables
+        obj.show();
+        A.show();
+        B.show();
+    }
+}
+
+
+// Class object MyClass
+// Class object MyClass
+// Class object MyClass
